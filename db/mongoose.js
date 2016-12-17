35ef21d0ce6c83d1ee03280542b6ac7465604fdc
Laci <elwindly@gmail.com>
2016-12-17 13:55:54 +0100

    Improving security

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 var env = process.env.NODE_ENV || 'development';
 
 
-process.env.MONGODB_URI = 'mongodb://elwindly:7ImmuraN500@ds050539.mlab.com:50539/todo';
+
 
 if(env ==='development'){
     process.env.MONGODB_URI = 'mongodb://localhost:27017/url';
